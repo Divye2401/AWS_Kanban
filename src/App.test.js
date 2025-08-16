7b92175887a73bec6504305fa60edca691a33c9a
Divye2401 <52701687+Divye2401@users.noreply.github.com>
2025-08-16 10:45:50 -0400

    shoudl work

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,25 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders kanban app header', () => {
+test("renders kanban app header", () => {
   render(<App />);
   const headerElement = screen.getByText(/Organize Your Tasks Efficiently/i);
   expect(headerElement).toBeInTheDocument();
 });
 
-test('renders todo board', () => {
+test("renders todo board", () => {
   render(<App />);
   const todoBoard = screen.getByText(/Todo/i);
   expect(todoBoard).toBeInTheDocument();
 });
 
-test('renders in progress board', () => {
+test("renders in progress board", () => {
   render(<App />);
   const inProgressBoard = screen.getByText(/In Progress/i);
   expect(inProgressBoard).toBeInTheDocument();
 });
 
-test('renders done board', () => {
+test("renders done board", () => {
   render(<App />);
   const doneBoard = screen.getByText(/Done/i);
   expect(doneBoard).toBeInTheDocument();
